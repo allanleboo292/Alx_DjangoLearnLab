a2d37569165ff6fd8a60694c946b35423ceccca5
Allan <allanleboo292@gmail.com>
2024-11-18 13:26:05 +0300

    creation of relationship_app

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,23 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    for book in books:
+        print(book.title)
+
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    for book in books:
+        print(book.title)
+
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    print(f"Librarian for {library.name}: {librarian.name}")
+
+if __name__ == "__main__":
+    books_by_author('J.K. Rowling')
+    books_in_library('City Library')
+    librarian_for_library('City Library')
