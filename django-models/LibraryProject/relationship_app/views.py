f52dfdde094e7e5effe9f01b8590a67b687498bb
Allan <allanleboo292@gmail.com>
2024-11-19 09:14:54 +0300

    Add function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book, Library
 
-
+from django.shortcuts import render
+from .models import Book
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.detail import DetailView
+from .models import Library
 
 
 def list_books(request):
@@ -15,3 +18,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a valid URL in your project
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
