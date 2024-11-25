b896d43d4acd6d610eefc9cc7272d24982586527
Allan <allanleboo292@gmail.com>
2024-11-25 20:43:11 +0300

    added import statement for serializers

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,7 @@
-from rest_framework import serializer
+from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         field = '__all__'
-        
