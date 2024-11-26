c98570ef7f7a883d6e5d29edf8ee962eeba78b27
Allan <allanleboo292@gmail.com>
2024-11-26 11:57:36 +0300

    added import statement for ModelViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book  # Import the Book model
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets.ModelViewSet import ModelViewSet
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Get all books from the database
     serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
