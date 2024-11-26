01a96de9816c149878f0d2d40179ed087dd1f437
Allan <allanleboo292@gmail.com>
2024-11-26 14:17:08 +0300

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
-from django.urls import path
+from django.urls import path, include
 from .views import BookList, BookViewSet  # Import your view
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Map '/books/' to the BookList view
@@ -14,4 +15,11 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+]
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
+
+urlpatterns = [
+    # other endpoints
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book  # Import the Book model
-from rest_framework.viewsets.ModelViewSet import ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Get all books from the database
     serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
 class BookViewSet(ModelViewSet):
-    queryset = user.object.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+from rest_framework.permissions import BasePermission
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.author == request.user
